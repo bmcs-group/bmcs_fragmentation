840da141f33e0aef050639265e701af4eed47f2e
rch <rostislav.chudoba@rwth-aachen.de>
2020-07-12 22:35:04 +0200

    fixed syntax of pmcm - plotting must be fixed

--- pmcm/main.py
+++ pmcm/main.py
@@ -2,10 +2,17 @@ import numpy as np
 import matplotlib.pylab as plt
 import pmcm
 
-mp = pmcm.ModelParams()
-po = pmcm.CrackBridgeRespSurface(mp=mp)
+mp = pmcm.ModelParams(m=100,T=1, sig_mu=3)
+cb = pmcm.CrackBridgeRespSurface(mp=mp)
+mc = pmcm.PMCM(mp=mp, cb_rs=cb)
 
-z = np.linspace(-2, 2, 100)
-sig_m = po.get_sig_m(z, 10)
-plt.plot(z, sig_m)
+if False:
+    z = np.linspace(-2, 2, 100)
+    sig_m = cb.get_sig_m(z, 10)
+    plt.plot(z, sig_m)
+    plt.show()
+
+fig, (ax, ax_sig_x) = plt.subplots(1,2,figsize=(8,3),tight_layout=True)
+ax_cs = ax.twinx()
+mc.plot(ax, ax_cs, ax_sig_x)
 plt.show()
--- pmcm/pmcm.py
+++ pmcm/pmcm.py
@@ -1,10 +1,12 @@
 """
 Probabilistic multiple cracking model
 """
+from typing import List, Any, Union
 
 import traits.api as tr
 import numpy as np
 import warnings
+from scipy.optimize import newton
 
 warnings.filterwarnings("error", category=RuntimeWarning)
 
@@ -25,6 +27,11 @@ class ModelParams(tr.HasTraits):
     L_x = tr.Float(500)
     sig_cu = tr.Float(20)
     sig_mu = tr.Float(10)
+    m = tr.Float(4)
+
+    @property
+    def Ec(self):
+        return self.Em * (1 - self.vf) + self.Ef * self.vf  # [MPa] mixture rule
 
 
 class PullOutModel(tr.HasTraits):
@@ -33,8 +40,6 @@ class PullOutModel(tr.HasTraits):
     at the loaded end
     """
     mp = tr.Instance(ModelParams)
-
-
 
 
 class CrackBridgeRespSurface(tr.HasTraits):
@@ -46,23 +51,20 @@ class CrackBridgeRespSurface(tr.HasTraits):
     mp = tr.Instance(ModelParams)
 
     def get_sig_m(self, sig_c: float, z: np.ndarray) -> np.ndarray:
-        """
-
-        :type sig_c: np.ndarray
+        """Get the profile of matrix stress along the specimen
+        :param z: np.ndarray
+        :type sig_c: float
         """
         mp = self.mp
         sig_m = np.minimum(z * mp.T * mp.vf / (1 - mp.vf), mp.Em * sig_c /
                            (mp.vf * mp.Ef + (1 - mp.vf) * mp.Em))
         return sig_m
 
-    def get_eps_f(self, sig_c):  # reinforcement strain (*\label{sig_f}*)
-        sig_m = get_sig_m(z, sig_c)
-        eps_f = (sig_c - sig_m * (1 - vf)) / vf / Ef
+    def get_eps_f(self, sig_c: float, z: np.ndarray) -> np.ndarray:
+        mp = self.mp
+        sig_m = self.get_sig_m(z, sig_c)
+        eps_f = (sig_c - sig_m * (1 - mp.vf)) / mp.vf / mp.Ef
         return eps_f
-
-    @property
-    def _get_Ec(self):
-        return self.Em * (1 - self.vf) + self.Ef * self.vf  # [MPa] mixture rule
 
 
 class PMCM(tr.HasTraits):
@@ -84,13 +86,14 @@ class PMCM(tr.HasTraits):
         :param sig_c_pre:
         :type sig_mu: float
         """
+        mp = self.mp
         # crack initiating load at a material element
         fun = lambda sig_c: sig_mu - self.cb_rs.get_sig_m(z, sig_c)
         try:  # search for the local crack load level
             return newton(fun, sig_c_pre)
         except (RuntimeWarning, RuntimeError):
             # solution not found (shielded zone) return the ultimate composite strength
-            return sig_cu
+            return mp.sig_cu
 
     def get_sig_c_K(self, z_x, x, sig_c_pre, sig_mu_x):
         # crack initiating loads over the whole specimen
@@ -100,17 +103,19 @@ class PMCM(tr.HasTraits):
         return sig_c_x[y_idx], x[y_idx]
 
     def get_cracking_history(self, update_progress=None):
-        x = np.linspace(0, self.L_x, self.n_x)  # specimen discretization (*\label{discrete}*)
-        sig_mu_x = self.sig_mu * np.random.weibull(m, size=self.n_x)  # matrix strength (*\label{m_strength}*)
+        mp = self.mp
+        x = np.linspace(0, mp.L_x, mp.n_x)  # specimen discretization
+        sig_mu_x: np.ndarray[np.float_] = mp.sig_mu * np.random.weibull(
+            mp.m, size=mp.n_x)  # matrix strength
 
-        XK = []  # recording the crack postions
-        sig_c_K = [0.]  # recording the crack initating loads
-        eps_c_K = [0.]  # recording the composite strains
-        CS = [self.L_x, self.L_x / 2]  # initial crack spacing
-        sig_m_x_K = [np.zeros_like(x)]  # stress profiles for crack states
+        XK: List[float] = []  # recording the crack postions
+        sig_c_K: List[float] = [0.]  # recording the crack initiating loads
+        eps_c_K: List[float] = [0.]  # recording the composite strains
+        CS: List[float] = [mp.L_x, mp.L_x / 2]  # initial crack spacing
+        sig_m_x_K: List[float] = [np.zeros_like(x)]  # stress profiles for crack states
 
-        Ec: float = self.cb_rs.Ec
-        Em: float = self.cb_rs.Em
+        Ec: float = mp.Ec
+        Em: float = mp.Em
         idx_0 = np.argmin(sig_mu_x)
         XK.append(x[idx_0])  # position of the first crack
         sig_c_0 = sig_mu_x[idx_0] * Ec / Em
@@ -121,7 +126,7 @@ class PMCM(tr.HasTraits):
             z_x = self.get_z_x(x, XK)  # distances to the nearest crack
             sig_m_x_K.append(self.cb_rs.get_sig_m(z_x, sig_c_K[-1]))  # matrix stress
             sig_c_k, y_i = self.get_sig_c_K(z_x, x, sig_c_K[-1], sig_mu_x)  # identify next crack
-            if sig_c_k == self.sig_cu:  # (*\label{no_crack}*)
+            if sig_c_k == mp.sig_cu:  # (*\label{no_crack}*)
                 break
             if update_progress:  # callback to user interface
                 update_progress(sig_c_k)
@@ -129,12 +134,29 @@ class PMCM(tr.HasTraits):
             sig_c_K.append(sig_c_k)  # corresponding composite stress
             eps_c_K.append(  # composite strain - integrate the strain field
                 np.trapz(self.cb_rs.get_eps_f(self.get_z_x(x, XK), sig_c_k), x) / np.amax(x))
-            XK_arr = np.hstack([[0], np.sort(np.array(XK)), [self.L_x]])
+            XK_arr = np.hstack([[0], np.sort(np.array(XK)), [mp.L_x]])
             CS.append(np.average(XK_arr[1:] - XK_arr[:-1]))  # crack spacing
 
-        sig_c_K.append(self.sig_cu)  # the ultimate state
-        eps_c_K.append(np.trapz(self.cb_rs.get_eps_f(self.get_z_x(x, XK), self.sig_cu), x) / np.amax(x))
+        sig_c_K.append(mp.sig_cu)  # the ultimate state
+        eps_c_K.append(np.trapz(self.cb_rs.get_eps_f(self.get_z_x(x, XK), mp.sig_cu), x) / np.amax(x))
         CS.append(CS[-1])
         if update_progress:
             update_progress(sig_c_k)
         return np.array(sig_c_K), np.array(eps_c_K), sig_mu_x, x, np.array(CS), np.array(sig_m_x_K)
+
+    def plot(self, ax, ax_cs, ax_sig_x):
+        sig_c_K, eps_c_K, sig_mu_x, x, CS, sig_m_x_K = self.get_cracking_history()
+        n_c = len(eps_c_K) - 2  # numer of cracks
+        ax.plot(eps_c_K, sig_c_K, marker='o', label='%d cracks:' % n_c)
+        ax.set_xlabel(r'$\varepsilon_\mathrm{c}$ [-]');
+        ax.set_ylabel(r'$\sigma_\mathrm{c}$ [MPa]')
+        ax_sig_x.plot(x, sig_mu_x, color='orange')
+        ax_sig_x.fill_between(x, sig_mu_x, 0, color='orange', alpha=0.1)
+        ax_sig_x.set_xlabel(r'$x$ [mm]');
+        ax_sig_x.set_ylabel(r'$\sigma$ [MPa]')
+        ax.legend()
+        eps_c_KK = np.array([eps_c_K[:-1], eps_c_K[1:]]).T.flatten()
+        CS_KK = np.array([CS[:-1], CS[:-1]]).T.flatten()
+        ax_cs.plot(eps_c_KK, CS_KK, color='gray')
+        ax_cs.fill_between(eps_c_KK, CS_KK, color='gray', alpha=0.2)
+        ax_cs.set_ylabel(r'$\ell_\mathrm{cs}$ [mm]');
